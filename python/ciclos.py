179a4f0eeef58bcfe8d850ab80a476de95894d97
Milton Figueredo Miles <gratno@gmail.com>
2024-07-26 07:11:56 -0500

    range

--- python/ciclos.py
+++ python/ciclos.py
@@ -160,19 +160,22 @@ longitud = len(nombre)
 # print(len(nombre))
 
 for i in range(5):
-    print("Hola")
+    print(i)
 
 print("------------")
 
-for i in range(0,5):
-    print("Hola")
+for i in range(1,5):
+    print(i)
 
 print("------------")
 
-for i in range(0,5,1):
-    print("Hola")
+for i in range(1,5,2):
+    print(i)
 
+print("------------")
 
+for i in range(10):
+    print(i)
 
 
 
