d8bf79908888f1722492391593bda0647c7503dd
Milton Figueredo Miles <gratno@gmail.com>
2024-07-28 18:46:22 -0500

    reto

--- python/reto.py
+++ python/reto.py
@@ -1,7 +1,16 @@
-# N como
+# N productos / N Suministros como
+# Informació adicional IVA
 # 
-# 
-# 
+# N cantidad productos a procesar
+# Para cada producto Ingresar
+# Código del producto.
+# Nombre al Producto
+# Cantidad comprada.
+# Valor unitario (Si IVA)
+# Tipo de Iva, que puede ser:
+#           1- Excento de IVA
+#           2- Bienes IVA 5%
+#           3- General IVA 19%    
 # 
 # 
 # 
