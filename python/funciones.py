ed5d5f1cd237e2bf67f90cc070231c567d586b85
Milton Figueredo Miles <gratno@gmail.com>
2024-07-30 13:35:39 -0500

    While Python break

---
+++ python/funciones.py
@@ -0,0 +1,22 @@
+# def pegarDuro():
+#     print("golpe")
+#     print("")
+
+# pegarDuro()    
+
+numero1 = input("Ingrse un numero: ")
+numero2 = input("Ingrse un numero: ")
+
+numero1 = int(numero1)
+numero2 = int(numero2)
+
+# Parámetros argumentos
+def sumar(numero1, numero2):
+    suma = numero1 + numero2
+    # return
+    print(suma)
+
+# Argumentos
+resultado = sumar(numero1, numero2)
+
+# print(f" s{resultado}")
