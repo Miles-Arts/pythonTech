43663c32c3583ce8242a653efa577a52dfcdd40e
Milton Figueredo Miles <gratno@gmail.com>
2024-07-29 21:45:46 -0500

    ordenando projet

--- python/calc.py
+++ python/calc.py
@@ -6,47 +6,43 @@
 (4)  División
 """
 
+print("---Elija una operación---")
 print("(1)  Suma")
 print("(2)  Resta")
 print("(3)  Multiplicación")
 print("(4)  División")
 
-# 
-
-# calculadora(op)
 
 op = input("Ingrese la operación: ")
 num1 = input("Ingrese valores: " )
 num2 = input("Ingrese valores: " )
 
 op = int(op)
-num1 = float(num1)
-num2 = float(num2)
+num1 = int(num1)
+num2 = int(num2)
 
-
+uno = "suma" 
 
 def calculadora(num1, num2, op):
-    
+   
     if op == 1:
-        print("Suma")
-        return num1 + num2
+        print(f"El resultado de la suma es: {num1 + num2}")
+        
+        return num1 + num2 
     elif op == 2:
         print()
-        return num1 + num2
+        return num1 - num2
     elif op == 3:
         print()
-        return num1 + num2
+        return num1 * num2
     elif op == 4:
         print()
-        return num1 + num2
+        return num1 / num2
     else:
         print(f"Ingresa un número correcto")
 
-
-# calculadora(45, 30, 3)  
 resultado = calculadora(num1, num2, op)  
-
 print(f"El resultado de la {resultado}")
 
-# valores ==
+
 
