f7891f7ccc61b830a7f055a3cdb1e0680b50cbdf
Milton Figueredo Miles <gratno@gmail.com>
2024-08-01 13:10:35 -0500

    Python agenda Stock

--- python/stock.py
+++ python/stock.py
@@ -9,3 +9,26 @@ def cargar():
         precio=float(input("Ingrese el precio: "))
         stock=int(input("Ingrese el stock actual: "))
         productos[codigo]=(descripcion,precio,stock)
+        continua=input("Desea cargar otro producto[s/n]")
+    return productos
+
+def imprimir(productos):
+    print("Listado completo de productos: ")
+    for codigo in productos:
+        print(codigo, productos[codigo][0],productos[codigo][1],productos[codigo][2])
+
+def consulta(productos):
+    codigo=int(input("Ingrese el código de artículo a consultar: "))
+    if codigo  in productos:
+        print(productos[codigo][0],productos[codigo][1],productos[codigo][2])
+
+def listado_stock_cero(productos):
+    print("Listado de artículos con stock en cero: ")
+    for codigo in productos:
+        if productos[codigo][2]==0:
+            print(productos[codigo][0],productos[codigo][1],productos[codigo][2])
+
+productos=cargar()
+imprimir(productos)
+consulta(productos)
+listado_stock_cero(productos)            
