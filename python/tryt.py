013a0af50a7475dfc3a462df2a681acbe7affb76
Milton Figueredo Miles <gratno@gmail.com>
2024-08-02 16:58:26 -0500

    Try  ValueError

--- python/tryt.py
+++ python/tryt.py
@@ -63,11 +63,62 @@
 # finally:
 #     print("El 'try except' ha terminado")        
 
-print("------------------")       
+# print("------------------")       
 
-try:
-    print(x)
-except NameError:
-    print("la variable x no está definida")
-finally:
-    print("El 'try except' ha terminado")  
+# try:
+#     print(x)
+# except NameError:
+#     print("la variable x no está definida")
+# finally:
+#     print("El 'try except' ha terminado")  
+
+
+
+# # x = -1
+# # if x < 0:
+# #     raise Exception("No hay número por debajo de cero")
+
+# x = "Hello"
+# if not type(x) is int:
+#     raise TypeError("sólo se permite números enteros")
+
+# Listas Operadores Especiales
+print("------------------")  
+
+lista=["a","b","c","d","a","b","c","d","a","b","c","d"]
+
+while True:
+    print("Nueva Busqueda")
+    letra=input("ingresa la letra a buscar: ")
+    indiceInicio=input("Indice para inicar búsqueda: ")
+    indiceFinal=input("Indice para terminar búsqueda: ")
+    noExiste=False
+    try:
+        indiceInicio=int(indiceInicio)
+        indiceFinal=int(indiceFinal)
+        try:
+            posicion=lista.index(letra, indiceInicio, indiceFinal)
+        except ValueError:
+            noExiste=True
+            raise 
+    except ValueError as mensajeError:
+        if(noExiste):
+            print(f"La letra {letra} no esta en la lista.")
+        else:
+            print("Solo se puede ingresar numeros.") 
+        print(f"Error: {mensajeError}")
+    else:
+        print(f"la letra \"{letra}\" está en la posición: ", posicion)
+        respuesta=None
+        while(respuesta!="2"):
+            print("¿Desea buscar otra letra? \n1.si\n2.no")
+            respuesta=input()
+            if(respuesta=="1"):
+                break
+            elif(respuesta=="2"):
+                continue       
+            else:
+                print("Opción no válida")
+        else:
+            break
+print("Busqueda finalizada")                        
