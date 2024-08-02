099f4194fafc2e62d9339081606f77d18bf0d8f6
Milton Figueredo Miles <gratno@gmail.com>
2024-08-02 13:02:53 -0500

    Lambda

--- python/lambda.py
+++ python/lambda.py
@@ -1,16 +1,67 @@
-# LAMBDAS
+# # LAMBDAS
 
-def doblar(numero):
-    resultado=numero*2
-    return resultado
+# print("---------------------")
+# # print("")
 
-def doblar(numero):
-    return numero*2
+# def doblar(numero):
+#     resultado=numero*2
+#     return resultado
 
-def doblar(numero): return numero*2
+# def doblar(numero):
+#     return numero*2
 
-lambda numero: numero*2
+# def doblar(numero): return numero*2
 
-doblar_numero=lambda numero: numero*2
+# lambda numero: numero*2
 
-print(doblar_numero(5))
+# doblar_numero=lambda numero: numero*2
+
+# print(doblar_numero(5))
+
+
+# print("---------------------")
+
+# impar=lambda numero: numero%2 != 0
+# print(impar(5)) 
+
+# revertir=lambda cadena: cadena[::-1]
+
+# print(revertir("hola"))
+
+# sumar = lambda x,y: x+y
+
+# print(sumar(5,2))
+
+
+# print("---------------------")
+
+# print("Calculadora")
+# print("Ingrese números a sumar")
+
+# numero1=int(input("Ingrese un numero: "))
+# numero2=int(input("Ingrese un numero: "))
+
+# sumar=lambda numero1,numero2: numero1 + numero2
+
+# print(f"Resultado: {sumar(numero1,numero2)}")
+# print(f"---Fin---")
+
+# print("---------------------")
+# print("---------------------")
+
+
+print("-----CALCULAR ÁREA RECTÁNGULO------")
+
+area_rectangulo=lambda base,altura: base*altura
+
+print(f"El área del rectágulo es: {area_rectangulo(15,10)}")
+
+print("-----------")
+print("----CALCULAR ÁREA DEL CIRCULO-------")
+
+radio=int(input("Ingrese el radio delcículo: "))
+area_circulo=lambda radio: (radio ** 2) *  3.141592 
+
+print(f"El área es: {area_circulo(radio)}")
+
+
