938d66893644ae8f61bf540fa144aaef181222d7
Milton Figueredo Miles <gratno@gmail.com>
2024-08-02 11:43:36 -0500

    Lambda

---
+++ python/lambda.py
@@ -0,0 +1,16 @@
+# LAMBDAS
+
+def doblar(numero):
+    resultado=numero*2
+    return resultado
+
+def doblar(numero):
+    return numero*2
+
+def doblar(numero): return numero*2
+
+lambda numero: numero*2
+
+doblar_numero=lambda numero: numero*2
+
+print(doblar_numero(5))
