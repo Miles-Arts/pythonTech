c5169906b6263b5ccaf2d90973e00bbbd603bf8c
Milton Figueredo Miles <gratno@gmail.com>
2024-08-02 07:55:44 -0500

    Try except

--- python/manejo_archivos.py
+++ python/manejo_archivos.py
@@ -1,5 +1,29 @@
+import json
 # JSON
 
+# [{"clave1": 1,"clave2":2}]
 
+# diccionario = {"nombre":"Rodolfo", "edad": 38, "ciudad": "cúcuta"}
+
+
+# # dato= json.dumps(diccionario, ensure_ascii=False)
+# dato= json.dumps(diccionario)
+
+# print(dato)
+
+diccionario={
+    "nombre":"Rodolfo", 
+    "edad": 38, 
+    "ciudad": "cúcuta",
+    "padres": ("jose", "luisa"),
+    "hijos": None,
+    "mascotas": [
+        {"tipo": "gato", "edad":2},
+        {"tipo": "perro", "edad":10}   
+    ]
+}
+
+datos= json.dumps(diccionario)
+print(datos)
 
 
