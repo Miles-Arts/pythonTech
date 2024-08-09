f157e05dbf873b59129aa5d96cb3a4ea26b7da05
Milton Figueredo Miles <gratno@gmail.com>
2024-08-09 06:45:29 -0500

    revew them

---
+++ python/repaso.py
@@ -0,0 +1,11 @@
+print("----------------------------")
+
+nombre_dia="Viernes"
+nombre_producto="Libro"
+precio=200
+iva=0.19
+estado=True
+
+print("El producto: " + nombre_producto + " tiene le precio de $" + str(precio))
+print("El producto:", nombre_producto , "tiene le precio de $" ,precio)
+print(f"El producto: {nombre_producto} tiene le precio de ${precio } ")
