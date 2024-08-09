8b75082a67a9404643d4d9dcfb72527028976c4a
Milton Figueredo Miles <gratno@gmail.com>
2024-08-09 07:17:22 -0500

    revew them

--- python/repaso.py
+++ python/repaso.py
@@ -6,24 +6,32 @@ precio=200
 iva=0.19
 estado=True
 
-print("El producto: " + nombre_producto + " tiene le precio de $" + str(precio))
-print("El producto:", nombre_producto , "tiene le precio de $" ,precio)
+# print("El producto: " + nombre_producto + " tiene le precio de $" + str(precio))
+# print("El producto:", nombre_producto , "tiene le precio de $" ,precio)
 print(f"El producto: {nombre_producto} tiene le precio de ${precio } ")
 
 precio_producto = precio + (precio * iva)
 precio_producto =  (precio * iva)
 
-print(f"Total a pagar es: {precio_producto }")
 
 print("----------------------------")
+print(f"Total a pagar es: '\'{precio_producto }'\'")
 
 # print(f"El total IVA: {precio_producto}")
 # print(f"El total IVA: {precio + precio_producto}")
 
 
-
-
-
+if precio > 200 and precio < 300:
+    print("Entre 200 y 300")
+elif precio > 200 and precio > 300:
+    print("Es mayor de 200 y 300")
+elif precio < 200 or precio == 200:
+    print("Es menor o igual")    
+else: 
+    if precio <= 100:
+        print("Menor o igual a 100")
+    else:
+        print("Es mayor")    
 
 
 
