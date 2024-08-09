18445c23d055357325447c557e4f7fff20d296af
Milton Figueredo Miles <gratno@gmail.com>
2024-08-09 07:02:36 -0500

    revew them

--- python/repaso.py
+++ python/repaso.py
@@ -9,3 +9,80 @@ estado=True
 print("El producto: " + nombre_producto + " tiene le precio de $" + str(precio))
 print("El producto:", nombre_producto , "tiene le precio de $" ,precio)
 print(f"El producto: {nombre_producto} tiene le precio de ${precio } ")
+
+precio_producto = precio + (precio * iva)
+precio_producto =  (precio * iva)
+
+print(f"Total a pagar es: {precio_producto }")
+
+print("----------------------------")
+
+# print(f"El total IVA: {precio_producto}")
+# print(f"El total IVA: {precio + precio_producto}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("----------------------------")
