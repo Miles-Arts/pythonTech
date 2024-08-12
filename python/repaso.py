b61a02100fbb5e4a85a675772ffc568d3246900d
Milton Figueredo Miles <gratno@gmail.com>
2024-08-12 13:01:02 -0500

    def Juego

--- python/repaso.py
+++ python/repaso.py
@@ -420,6 +420,37 @@ import os
 
 print("-"*30)
 
+def guardar_puntajes(nombre_archivo, puntajes):
+    archivo=open(nombre_archivo, "w")
+
+    for nombre, puntaje, tiempo in puntajes:
+
+        archivo.write(nombre+","+str(puntaje)+","+tiempo+"\n")
+
+    archivo.close()
+
+def recuperar_puntajes(nombre_archivo):
+
+    puntajes=[]
+    archivo=open(nombre_archivo, "r")
+
+    for linea in archivo:
+        nombre, puntaje, tiempo = linea.rstrip("\n").split(",")
+        puntajes.append((nombre, int(puntaje), tiempo))
+
+    archivo.close()
+
+    return puntajes
+
+
+
+
+
+
+
+
+
+
 
 
 
