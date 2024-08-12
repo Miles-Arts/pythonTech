ad217e9dffe5d4a0e0d8ca722d52f266c9cd8e00
Milton Figueredo Miles <gratno@gmail.com>
2024-08-12 07:01:26 -0500

    file

---
+++ python/ejercicios/ejercicio1.py
@@ -0,0 +1,8 @@
+num1= float(input("Ingrese el primer números: "))
+num1= float(input("Ingrese el segundo números: "))
+num1= float(input("Ingrese el tercer números: "))
+
+
+nombre = input("ingrese edad")
+
+
