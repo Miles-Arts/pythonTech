2097fd8fc248ae372d54bd048b090021797b6d49
Milton Figueredo Miles <gratno@gmail.com>
2024-08-13 14:36:42 -0500

    Factorial

--- python/ejercicios/ejercicio8.py
+++ python/ejercicios/ejercicio8.py
@@ -1,3 +1,4 @@
+import math
 # Enunciado 
 # Factorial: Realizar un programa que calcule 
 # el factorial de un número que ingrese un usuario, 
@@ -5,3 +6,30 @@
 # debe pedir el número tantas veces como sea necesario 
 # hasta que cumpla la condición. Recuerda usar la librería math
 # 
+
+print("Ingrese los numero para calcular factorial")
+print("Ingrese un número entre > 0 y < 20")
+
+numero = int(input("Número: "))
+
+if numero > 0 and numero < 20:
+    resultado = math.factorial(numero)
+    print(f"El factorial de {numero} es: {resultado}")
+elif numero >= 21:
+    print("Ingrese un número entre > 0 y < 20")
+
+
+# math.factorial
+
+
+# while True:
+#     if numero > 0 and numero < 20:
+#         for numero in range(1, numero, +1):
+#             resultado =  numero * numero 
+#             print(f"hola {resultado}")
+#         break    
+#     elif numero >= 21:
+#         print("Ingrese un número entre > 0 y < 20")
+#         break
+              
+#     continue
