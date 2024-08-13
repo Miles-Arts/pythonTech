c3f8a32cd918da4f7f4f7ab7be170ea15da1a1e1
Milton Figueredo Miles <gratno@gmail.com>
2024-08-13 13:09:26 -0500

    Exercise on Python

---
+++ python/ejercicios/ejercicio7.py
@@ -0,0 +1,16 @@
+# Enunciado 
+# Realizar un programa que pida al usuario digitar un día de la semana
+# * Si es lunes, sábado o domingo imprimir el día de la semana y un mensaje
+# * Si es viernes imprima el día de la semana y “Ya vemos cerca el fin de semana”
+# De lo contrario imprima “Avancemos que ya es”, día de la semana
+# 
+
+dia_semana = str(input("Ingrese un día de la semana: ").lower())
+
+
+if dia_semana == "viernes":
+    print(f"¡Es {dia_semana} ya vemos cerca el fin de semana!")
+elif dia_semana == "jueves" or  dia_semana == "miercoles":
+    print(f"¡Es {dia_semana} si se puede!")
+elif dia_semana == "lunes" or  dia_semana == "sabado" or  dia_semana == "domingo":
+    print(f"Avancemos que ya es {dia_semana}")    
