9ca333f5d4dd24c94ee333f0892dce5fc08eca97
Milton Figueredo Miles <gratno@gmail.com>
2024-08-13 12:39:36 -0500

    Exercise on Python

---
+++ python/ejercicios/ejercicio6.py
@@ -0,0 +1,41 @@
+#   Enunciado Área: 
+# Escriba un programa que 
+# primero si se quiere calcular el área 
+# de un triángulo (T o t) o la de un círculo (C o c). 
+# Si contesta que quiere calcular el área de un triángulo 
+# solicite base y altura, de lo contrario 
+# solicite el radio e imprima el área correspondiente
+
+
+print("¿Cuál Área desea calcular?")
+pregunta = str(input("Triángulo (T o t) o la de un Círculo (C o c): "))
+
+respuesta = pregunta.lower()
+
+while True:
+    if respuesta == "t":
+        print("Ingrese las medidas para calcular área.")
+        medida1 = int(input("Ingrese la base: "))
+        medida2 = int(input("Ingrese la altura: "))
+        # medida3 = int(input("Medida 3: "))
+        area = (medida1 * medida2) / 2
+        print(f"El área del Triángulo es: {area}")
+        break
+
+    elif respuesta == "c":
+        print("Ingrese el radio del Círculo.")
+        medida1 = int(input("Ingresea  radio: "))
+        area = (medida1 ** 2) * 3.14
+        print(f"El áreas del Círculo es: {area}")
+        break
+    elif respuesta != "t" and respuesta != "c":
+        print("Ingrese un parámetro válido")
+    continue
+
+
+
+
+
+
+
+
