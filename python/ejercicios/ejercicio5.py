978601092faf6cd872bbb662c456b3fd19efd1bd
Milton Figueredo Miles <gratno@gmail.com>
2024-08-13 11:50:28 -0500

    Exercise on Python

--- python/ejercicios/ejercicio5.py
+++ python/ejercicios/ejercicio5.py
@@ -1,16 +1,23 @@
 # Enunciado Múltiplo: Escriba un programa que pida 
 # dos números enteros e imprima si el mayor es múltiplo del menor
 
-numero1 = int(input("Ingrese el primer número entero: "))
-numero2 = int(input("Ingrese el segundo número entero: "))
+num1 = int(input("Ingrese el primer número entero: "))
+num2 = int(input("Ingrese el segundo número entero: "))
 
-if numero1 > numero2:
-    resultado = numero1 % numero2
+if num1 > num2:
+    resultado = num1 % num2
     if  resultado == 0:
-        print(f"El número mayor { numero1 } es multiplo de {numero2} y resultado {resultado}")
+        print(f"El número mayor { num1 } es multiplo de {num2} y resultado {resultado}")
          
-elif numero1 < numero2:
-    resultado = numero2 % numero1
+elif num1 < num2:
+    resultado = num2 % num1
     if resultado == 0:
-        print(f"El número mayor { numero2 } es multiplo de {numero1} y resultado {resultado}")
+        print(f"El número mayor { num2 } es multiplo de {num1} y resultado {resultado}")
+
+# if num1 > num2 and num1 % num2 == 0:
+#     print(f"{num1} es múltiplo de {num2}.")
+# elif num2 > num1 and num2 % num1 == 0:
+#     print(f"{num2} es múltiplo de {num1}.")
+# else:
+#     print("El mayor no es múltiplo del menor.")
         
