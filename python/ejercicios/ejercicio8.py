68b8139d08c37e91e5c0a0fbe64424d54276fa9e
Milton Figueredo Miles <gratno@gmail.com>
2024-08-13 13:20:11 -0500

    Exercise on Python

---
+++ python/ejercicios/ejercicio8.py
@@ -0,0 +1,7 @@
+# Enunciado 
+# Factorial: Realizar un programa que calcule 
+# el factorial de un número que ingrese un usuario, 
+# el número debe ser > 0 y < 20, de no ser así, 
+# debe pedir el número tantas veces como sea necesario 
+# hasta que cumpla la condición. Recuerda usar la librería math
+# 
