d9c8e404f0553ebdeed0b347fc6b08a472584ef0
Milton Figueredo Miles <gratno@gmail.com>
2024-08-13 21:31:01 -0500

    sum letras

--- python/ejercicios/ejercicio9.py
+++ python/ejercicios/ejercicio9.py
@@ -7,21 +7,44 @@
 
 
 
-frase = str(input("Escribe una frase: ").lower())
-letra = str(input("Ingrese una letra: "))
+# frase = str(input("Escribe una frase: ").lower())
+# letra = str(input("Ingrese una letra: "))
 
-for letras in frase:
-    letras.split()
-    # print(letras)
-    if letras == letra:
-        numero_letras = len(letras)
-        sum(numero_letras)
-        print(f"Hay {numero_letras} letras de la letra {letras}")
-        break    
+# for letras in frase:
+#     letras.split()
+#     # print(letras)
+#     if letras == letra:
+#         numero_letras = len(letras)
+#         sum(numero_letras)
+#         print(f"Hay {numero_letras} letras de la letra {letras}")
+#         break    
+# else:
+#     print("no hay letra")    
+
+hola = "holoholo"
+letra = str(input("ingresa una letra: "))
+
+hola = hola.split()
+
+holas = []
+
+holas.append(hola)
+
+# print(len(holas))
+
+# palabra = len(hola)
+
+for letras in range(0, len(hola), 1):
+        if letras == letra:
+              # sum(letra)
+            print(f"Encontramos  la letra {letra}")
+        break
+        
 else:
-    print("no hay letra")    
+    print(f"no hay letra {letra} ")
 
 
+# print(palabra)
 
 
 
