de8db6ea91b43949540796ef0902ada21e284de4
Milton Figueredo Miles <gratno@gmail.com>
2024-08-16 06:39:53 -0500

    CRUD with Python

---
+++ proyecto/backend/main.py
@@ -0,0 +1,17 @@
+datosAves=[
+    {"especie": "Águila", "departamento": "Boyacá", "avistamiento": 5},
+    {"especie": "Colibrí", "departamento": "Cundinamarca", "avistamiento": 10},
+    {"especie": "ÁTucán", "departamento": "Boyacá", "avistamiento": 8}
+]
+
+def mostrarAves():
+    print("Mostrar Aves")
+    print(datosAves)
+
+
+mostrarAves()    
+
+
+
+
+
