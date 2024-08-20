4b12f0f3d01fa88692e592166f51342d681448e8
Milton Figueredo Miles <gratno@gmail.com>
2024-08-20 15:45:32 -0500

    Crud error solution

--- python/ciudades.py
+++ python/ciudades.py
@@ -1,16 +1,22 @@
 
 
 ciudades = [
-    {"ciudad": "bogota", "departamento": "cundinamarca", "clima": "frío"},
-    {"ciudad": "medellin", "departamento": "antioquia", "clima": "templado"},
-    {"ciudad": "tunja", "departamento": "boyacá", "clima": "frío"}
+    {"Ciudad": "Bogotaá", "Departamento": "Cundinamarca", "clima": "frío"},
+    {"Ciudad": "Medellín", "Departamento": "Antioquia", "clima": "templado"},
+    {"Ciudad": "Tunja", "Departamento": "Boyacá", "clima": "frío"}
 ]
 
-def mostrar_ciudaes():
+def mostrar_ciudades():
     print("---Ciudades---")
     print(ciudades)
 
-def eliminar_elemento():
+def anadir_ciudad():
+    print("--Añadir Ciudad--")
+
+def editar_ciudad():
+    print("--Editar Ciudad--")    
+
+def eliminar_ciudad():
     print("---Eliminador---")
     sin_eliminar = str(input("Ingrese el valor a eliminar: "))
     cantidad_casas_anterior = len(ciudades)
@@ -21,8 +27,42 @@ def eliminar_elemento():
         ("Ingrese un parametro válido")    
     print(ciudades)
 
+def menu_ciudades():
+    while True:
+      print("---Menu Ciudades---")  
+      print("")
+      print("1. Mostrar Ciudades")
+      print("2. Añadir Ciudades")
+      print("3. Editar Ciudades")
+      print("4. Eliminar Ciudades")
+      print("5. Salir del menú")
+      print("")
+      opcion = int(input("Ingrese número: "))
+
+      if opcion == 1:
+            mostrar_ciudades()
+      elif opcion == 2:
+            anadir_ciudad()
+      elif opcion == 3:
+            editar_ciudad()
+      elif opcion == 4:
+            eliminar_ciudad()
+      elif opcion == 4:
+           print("")
+           print("---Fin---")   
+      else:
+           print("Ingrese un parámetro válido")                    
+
+
+     
 
 
 
 
-eliminar_elemento()
+
+
+mostrar_ciudades()
+
+
+
+eliminar_ciudad()
