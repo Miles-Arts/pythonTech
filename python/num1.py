bc5eea3610b2609cbb6efb736bc5fba99dfd0ea2
Milton Figueredo Miles <gratno@gmail.com>
2024-08-23 10:57:21 -0500

    numpy

---
+++ python/num1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+a = np.array([1,2,3])
+print(a)
+
+escalaraProducto = a * 2
+print(escalaraProducto)
+
+seno = np.sin(a)
+print(a)
+
+logaritmo = np.log(a)
+print(logaritmo)
