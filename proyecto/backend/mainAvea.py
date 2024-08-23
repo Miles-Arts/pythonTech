05363a1abaf5004ad15f906095a16496d109c606
Milton Figueredo Miles <gratno@gmail.com>
2024-08-22 19:52:59 -0500

    Project Birds

--- proyecto/backend/mainAvea.py
+++ proyecto/backend/mainAvea.py
@@ -8,8 +8,13 @@ datosAves=[
             "comportamiento": "Se alimenta de neétar"
         },
         "multimedia": {
-            "fotos": ["ruta/foto1.jpg","ruta/foto2.jpg" ]
+            "fotos": ["ruta/foto1.jpg","ruta/foto2.jpg" ],
+            "sonidos": ["ruta/sonidos1.mp3"]
         },
+        "observaciones": [
+            {"fecha": "2024-05-01", "lugar": "Boyacá", "avistamientos": 5},
+            {"fecha": "2024-04-01", "lugar": "Cundinamarca", "avistamientos": 10}
+        ] 
     }
 ]
 
