fe4955c2c0bc7da0e2913dad2c9107f7f35bafe0
Milton Figueredo Miles <gratno@gmail.com>
2024-08-23 15:55:55 -0500

    Arboles project

---
+++ python/arbolTuta.py
@@ -0,0 +1,16 @@
+datosArboles=[
+    {
+        "nombreComun": "Duraznillo",
+        "descripcion": {
+            "tamano": "20 mt",
+            "color": "Verde",
+            "caracteristicas": "Mediano",
+            "comportamiento": "Crecimiento rápido"
+        },
+        "multimedia": {
+            "fotos": [],
+            "": [],
+        }
+
+    }
+]
