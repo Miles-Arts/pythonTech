9c5c9ba0161db54941e9c1f74c65a64157444c0c
Milton Figueredo Miles <gratno@gmail.com>
2024-08-22 19:44:55 -0500

    Project Birds

--- proyecto/backend/mainAvea.py
+++ proyecto/backend/mainAvea.py
@@ -1,7 +1,31 @@
 datosAves=[
     {
-        "nombre": "Colibrí",
-        "",
+        "nombreComun": "Colibrí", 
+        "descripcion": {
+            "tamaño": "9 cm",
+            "color": "Verde y Rojo",
+            "caracteristicas": "Pequeño",
+            "comportamiento": "Se alimenta de neétar"
+        },
+        "multimedia": {
+            "fotos": ["ruta/foto1.jpg","ruta/foto2.jpg" ]
+        },
     }
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
