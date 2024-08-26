bb9dcf3735220c446bb0cc66829d2702ee797289
Milton Figueredo Miles <gratno@gmail.com>
2024-08-26 06:57:40 -0500

    pandas

--- python/pandas/seriesPanda.py
+++ python/pandas/seriesPanda.py
@@ -1,17 +1,22 @@
 import csv
-import pandas as pn
+import pandas as pd
 
 # nombres=input("Ingrese los nombres: ").split(",")
 # print(nombres)
 # print(",".join(nombres))
 
-with open("archivoP.csv", "r", encoding="utf-8") as archivo:
-    data=csv.reader(archivo)
-    for fila in data:
-        print(fila)
+# with open("archivoP.csv", "r", encoding="utf-8") as archivo:
+#     data=csv.reader(archivo)
+#     print(data)
+#     for fila in data:
+#         print(fila)
+#         # print(fila[0],fila[1],fila[2])
+#         print(" - ".join(fila))
 
 
-
+# with open("archivoP.csv", "w", encoding="utf-8") as archivo:
+#     escribir=csv.writer(archivo)
+#     escribir.writerow([ "Rodolfo","Lucas", "34465899" ])
 
 
 
