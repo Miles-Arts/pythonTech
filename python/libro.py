09639fafb67a97d9dfd3f3ab3066318e31d5da0f
Milton Figueredo Miles <gratno@gmail.com>
2024-08-28 10:49:35 -0500

    Operator Morsa

--- python/libro.py
+++ python/libro.py
@@ -31,118 +31,134 @@ color1="#AF549B"
 
 
 
-point = (2, 5)
+# point = (2, 5)
 
-match point:
-    case (x,y):
-        print(f"({x},{y}) is in plane")
-    case (x,y,z):
-        print(f"({x},{y},{x}) is in space")
+# match point:
+#     case (x,y):
+#         print(f"({x},{y}) is in plane")
+#     case (x,y,z):
+#         print(f"({x},{y},{x}) is in space")
 
-point = (3,1,7)
+# point = (3,1,7)
 
-match point:
-    case (x,y):
-        print(f"({x},{y}) is in plane")
-    case (x,y,z):
-        print(f"({x},{y},{z}) is in plane")
+# match point:
+#     case (x,y):
+#         print(f"({x},{y}) is in plane")
+#     case (x,y,z):
+#         print(f"({x},{y},{z}) is in plane")
 
-print("-"*60)
+# print("-"*60)
 
-point2 = ("2","6")
+# point2 = ("2","6")
 
-match point2:
-    case (x,y):
-        print(f"({x},{y}) is in plane")
-    case (x,y,z):
-        print(f"({x},{y},{z}) is in space")
-    case _:
-        print("Unknown ")        
-
-
-
-print("-"*60)
+# match point2:
+#     case (x,y):
+#         print(f"({x},{y}) is in plane")
+#     case (x,y,z):
+#         print(f"({x},{y},{z}) is in space")
+#     case _:
+#         print("Unknown ")        
 
 
-point3 = ("2", "5")
 
-match point3:
-    case (int(), int()):
-        print(f"({point}) is in plane")
-    case (int(),int(),int())  :
-        print(f"({point}) is in space")
-    case _:
-        print("Unknown!")      
+# print("-"*60)
 
 
-print("-"*60)
+# point3 = ("2", "5")
 
-point4 = (3,1,9)
+# match point3:
+#     case (int(), int()):
+#         print(f"({point}) is in plane")
+#     case (int(),int(),int())  :
+#         print(f"({point}) is in space")
+#     case _:
+#         print("Unknown!")      
 
-match point4:
-    case (int(),int()):
-        print(f"({point}) is in plane")
-    case (int(),int(),int()):
-        print(f"({point4}) is in space")
-    case _:
-        print("Unknown!")        
 
-print("-"*60)
+# print("-"*60)
 
-point5 = (8,3,5)
-match point5:
-    case (int(x),int(y)):
-        dist_to_origin =(x ** 2 + y ** 2) ** (1 / 2)
-        print(dist_to_origin)
-    case (int(x), int(y), int(z)):
-        dist_to_origin =(x ** 2 + y ** 2 + z **2) ** (1 / 2)
-        print(dist_to_origin)
-    case _:
-        print("Unknown!")
+# point4 = (3,1,9)
 
-print("-"*60)
+# match point4:
+#     case (int(),int()):
+#         print(f"({point}) is in plane")
+#     case (int(),int(),int()):
+#         print(f"({point4}) is in space")
+#     case _:
+#         print("Unknown!")        
 
-point6 = ("8",3,6)
-match point6:
-    case (int(x), int(y)):
-        dist_to_origin = (x ** 2 + y ** 2) ** (1 / 2) 
-        print(dist_to_origin)
-    case (int(x), int(y), int(z)):
-        dist_to_origin =(x ** 2 + y ** 2 + z **2) ** (1 / 2)
-        print(dist_to_origin)
-    case _:
-        print("Unknown!")
+# print("-"*60)
 
-print("-"*60)
+# point5 = (8,3,5)
+# match point5:
+#     case (int(x),int(y)):
+#         dist_to_origin =(x ** 2 + y ** 2) ** (1 / 2)
+#         print(dist_to_origin)
+#     case (int(x), int(y), int(z)):
+#         dist_to_origin =(x ** 2 + y ** 2 + z **2) ** (1 / 2)
+#         print(dist_to_origin)
+#     case _:
+#         print("Unknown!")
 
-#Lista de diccionarios
+# print("-"*60)
 
-auths=[
-    {"user_name": "juan", "password": "1234"},
-    {"email": "juan@gmail.com", "password": "43212"},
-    {"email": "tes@gmail.com", "password": "ABCD"},
-    {"user_name": "juan", "password": 1234}
-]
+# point6 = ("8",3,6)
+# match point6:
+#     case (int(x), int(y)):
+#         dist_to_origin = (x ** 2 + y ** 2) ** (1 / 2) 
+#         print(dist_to_origin)
+#     case (int(x), int(y), int(z)):
+#         dist_to_origin =(x ** 2 + y ** 2 + z **2) ** (1 / 2)
+#         print(dist_to_origin)
+#     case _:
+#         print("Unknown!")
 
-for auth in auths:
-    print(auth)
-    match auth:
-        case {"user_name": str(user_name), "password": str(password)}:
-            print("Authenticating witch username and password")
-            print(f"{user_name}:{password}")
-        case {"email": str(email), "token": str(token)}:
-            print("Authenticating with email and token")
-            print(f"{email}:{token}")
-        case _:
-            print("Authenticathing method not valid!")
-    print("-"*10)        
+# print("-"*60)
+
+# #Lista de diccionarios
+
+# auths=[
+#     {"user_name": "juan", "password": "1234"},
+#     {"email": "juan@gmail.com", "password": "43212"},
+#     {"email": "tes@gmail.com", "password": "ABCD"},
+#     {"user_name": "juan", "password": 1234}
+# ]
+# for auth in auths:
+#     print(auth)
+#     match auth:
+#         case {"user_name": str(user_name), "password": str(password)}:
+#             print("Authenticating witch username and password")
+#             print(f"{user_name}:{password}")
+#         case {"email": str(email), "token": str(token)}:
+#             print("Authenticating with email and token")
+#             print(f"{email}:{token}")
+#         case _:
+#             print("Authenticathing method not valid!")
+#     print("-"*10)        
              
+# print("-"*60)
+
+# age=21
+
+# match age:
+#     case 0 | None:
+#         print("Not a person")
+#     case n if n < 17:
+#         print("Nop")
+#     case n if n < 22:
+#         print("Not is the USA")
+#     case _:
+#         print("Yes!")
 
 
+# Operador MORSE
+print("-"*60)
 
-
-
-
+radius=3.5
+perimeter= 2 * 3.14 * radius
+if perimeter < 100:
+    print("Increase radius to reach minimunperimeter")
+    print("Actual perimeter: ", perimeter)
 
 
 
