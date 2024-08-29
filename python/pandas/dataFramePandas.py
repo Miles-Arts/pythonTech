cc5945d9a558bcf5425743c5fe7cb4c56eb20d1c
Milton Figueredo Miles <gratno@gmail.com>
2024-08-28 20:41:30 -0500

    Pandas

--- python/pandas/dataFramePandas.py
+++ python/pandas/dataFramePandas.py
@@ -1,31 +1,37 @@
 import pandas as pd
+import numpy as np
+# datos1=[
+#     [1, "Ana"],
+#     [2, "Juan"],
+#     [3, "Maria"]
+# ]
 
-datos1=[
-    [1, "Ana"],
-    [2, "Juan"],
-    [3, "Maria"]
-]
+# # print(datos)
 
-# print(datos)
+# df1=pd.DataFrame(datos1, index=["a", "b", "c"], columns=["ID", "Nombre"])
+# print(df1)
 
-df1=pd.DataFrame(datos1, index=["a", "b", "c"], columns=["ID", "Nombre"])
-print(df1)
+# print("-"*60)
+
+# datos2={"ID":[1,2,3], "Nombre": ["Ana", "Juan", "Maria"]}
+# df2=pd.DataFrame(datos2)
+# print(df2)
+
+# nombres=pd.Series(["Ana", "Juan", "Maria"])
+# edades=pd.Series([10, 15, 20])
+# ciudades=pd.Series(["Bogota", "Cucuta", "Medellin"])
+
+# df4=pd.DataFrame({"Nombres": nombres, "Edades": edades, "Ciudades": ciudades})
+# print(df4)
 
 print("-"*60)
 
-datos2={"ID":[1,2,3], "Nombre": ["Ana", "Juan", "Maria"]}
-df2=pd.DataFrame(datos2)
-print(df2)
-
-nombres=pd.Series(["Ana", "Juan", "Maria"])
-edades=pd.Series([10, 15, 20])
-ciudades=pd.Series(["Bogota", "Cucuta", "Medellin"])
-
-df4=pd.DataFrame({"Nombres": nombres, "Edades": edades, "Ciudades": ciudades})
-print(df4)
-
-
-
+datos={
+    "Dia": ["Lunes", "Martes", "Miercoles", "Martes", "Viernes", np.nan, "Viernes"],
+    "Ventas": [100, 150, np.nan, 150, 200, 130, 200],
+    "Vendedor": ["Ana", "Juan", "Ana", "Juan", "Ana", "Maria", "Ana"],
+    "Ciudad": ["Bogota", "Cucuta", np.nan, "Cucuta", "Bogota", "Bogota","Bogota", ]
+}
 
 
 
