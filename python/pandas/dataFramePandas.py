2ae48a73255a0a3c0eed9f6644d3c510041ba2a7
Milton Figueredo Miles <gratno@gmail.com>
2024-08-28 20:29:39 -0500

    Pandas

--- python/pandas/dataFramePandas.py
+++ python/pandas/dataFramePandas.py
@@ -1,16 +1,27 @@
 import pandas as pd
 
-datos=[[1, "Ana"],[3, "Juan"],[3, "Maria"]]
-print(datos)
+datos1=[
+    [1, "Ana"],
+    [2, "Juan"],
+    [3, "Maria"]
+]
 
+# print(datos)
 
+df1=pd.DataFrame(datos1, index=["a", "b", "c"], columns=["ID", "Nombre"])
+print(df1)
 
+print("-"*60)
 
+datos2={"ID":[1,2,3], "Nombre": ["Ana", "Juan", "Maria"]}
+df2=pd.DataFrame(datos2)
+print(df2)
 
+nombres=pd.Series(["Ana", "Juan", "Maria"])
+edades=pd.Series([10, 15, 20])
+ciudad=pd.Series(["Bogota", "Cucuta", "Medellin"])
 
-
-
-
+df4=pd.DataFrame({})
 
 
 
