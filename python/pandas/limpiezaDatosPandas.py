f4ea2037e992a243b72fdf12280c1687168932dd
Milton Figueredo Miles <gratno@gmail.com>
2024-08-30 07:52:43 -0500

    Limpieza Data

--- python/pandas/limpiezaDatosPandas.py
+++ python/pandas/limpiezaDatosPandas.py
@@ -17,14 +17,40 @@ df=pd.DataFrame(data)
 df_dropped=df.dropna()
 # print(df_dropped)
 
-df=df.dropna(subset=["Fechas"])
+# df=df.dropna(subset=["Fechas"])
 # print(df)
 
+# df_cleaned=df.dropna(axis=1)
+# # print(df_cleaned)
 
-df["Ventas"]=df["Ventas"].fillna(df["Ventas"].mean())
+# df["Ventas"]=df["Ventas"].fillna(df["Ventas"].mean())
+# # print(df)
+
+# df["Vendedores"]=df["Vendedores"].fillna(df["Vendedores"].mode()[0])
+# # print(df)
+
+print("-"*60)
+
+df["Vendedores"].fillna("Carlos", inplace=True)
+# df=df.drop_duplicates()
 print(df)
 
+# df["Fechas"]=pd.to_datetime(df["Fechas"])
+# # print(df["Fechas"])
+# # # print(df.info())
 
+# df["Ventas"]=df["Ventas"].astype(int)
+# print(df["Ventas"])
+
+
+# df["Ciudades"]=df["Ciudades"].astype("category")
+# # print(df["Ciudades"])
+
+# print(df.dtypes)
+
+
+df.replace({"Cucuta": "Cúcuta"}, inplace=True)
+print(df)
 
 
 
