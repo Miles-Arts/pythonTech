e321b30e37c8c3eedf14ded8b74335b31a0ced77
Milton Figueredo Miles <gratno@gmail.com>
2024-08-29 19:30:14 -0500

    DataFrame Pandas

---
+++ python/pandas/ventasD3.py
@@ -0,0 +1,27 @@
+import pandas  as pd
+import numpy as np
+
+datos_ventas={"Día":    ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"], 
+              "Ventas": [450,         300,      200,      400,         600  ]}
+
+df_ventas =pd.DataFrame(datos_ventas)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
