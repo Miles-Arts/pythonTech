32d1f94b7283b3d99de07b579c75fdca6bc7cb0b
Milton Figueredo Miles <gratno@gmail.com>
2024-08-30 07:18:24 -0500

    Limpieza Data

--- python/pandas/dataFramePandas.py
+++ python/pandas/dataFramePandas.py
@@ -1,93 +1,93 @@
-import pandas as pd
-import numpy as np
-# datos1=[
-#     [1, "Ana"],
-#     [2, "Juan"],
-#     [3, "Maria"]
-# ]
+# import pandas as pd
+# import numpy as np
+# # datos1=[
+# #     [1, "Ana"],
+# #     [2, "Juan"],
+# #     [3, "Maria"]
+# # ]
 
-# # print(datos)
+# # # print(datos)
 
-# df1=pd.DataFrame(datos1, index=["a", "b", "c"], columns=["ID", "Nombre"])
-# print(df1)
+# # df1=pd.DataFrame(datos1, index=["a", "b", "c"], columns=["ID", "Nombre"])
+# # print(df1)
 
-# print("-"*60)
+# # print("-"*60)
 
-# datos2={"ID":[1,2,3], "Nombre": ["Ana", "Juan", "Maria"]}
-# df2=pd.DataFrame(datos2)
-# print(df2)
+# # datos2={"ID":[1,2,3], "Nombre": ["Ana", "Juan", "Maria"]}
+# # df2=pd.DataFrame(datos2)
+# # print(df2)
 
-# nombres=pd.Series(["Ana", "Juan", "Maria"])
-# edades=pd.Series([10, 15, 20])
-# ciudades=pd.Series(["Bogota", "Cucuta", "Medellin"])
+# # nombres=pd.Series(["Ana", "Juan", "Maria"])
+# # edades=pd.Series([10, 15, 20])
+# # ciudades=pd.Series(["Bogota", "Cucuta", "Medellin"])
 
-# df4=pd.DataFrame({"Nombres": nombres, "Edades": edades, "Ciudades": ciudades})
-# print(df4)
-
-print("-"*60)
-
-datos={
-    "Dia":      ["Lunes", "Martes", "Miercoles", "Martes", "Viernes", np.nan, "Viernes" ],
-    "Ventas":   [   100,    150,     np.nan,      150,      200,      130,     200      ],
-    "Vendedor": ["Ana",   "Juan",   "Ana",       "Juan",    "Ana",    "Maria", "Ana"    ],
-    "Ciudad":   ["Bogota","Cucuta",  np.nan,     "Cucuta",  "Bogota", "Bogota","Bogota",]
-}
-
-df=pd.DataFrame(datos)
-# print(df)
-
-
-# print(df.head())
-# print(df.describe())
-# print(df["Ventas"])
-# # print(df["Ciudad"])
-# print(df.iloc[2])
-# print(df.iloc[5])
-# print(df[ df["Ventas"] > 150 ])
-# print(df[ df["Ciudad"] == "Cucuta" ])
-# print(df.loc[ df["Ventas"] > 100, ["Dia", "Ventas"]])
-# print("-"*30)
-# print(df.iloc[ 1 :3, 1:3  ])
-# print(df.groupby("Vendedor")["Ventas"].sum())
-# print("-"*30)
-# print(df.isnull().sum())
-
-# print("-"*30)
-
-# print(df)
-print("-"*30)
-# df_sin_nulos=df.dropna()
-# print(df_sin_nulos)
-
-# df["Ventas"]=df["Ventas"].fillna(df["Ventas"].mean())
-# # print(df)
-
-# df["Ventas"]=df["Ventas"].astype(int)
-# # print(df)
-
-# df=df.drop_duplicates()
-# print(df)
-
-df["Ciudad"]=df["Ciudad"].astype("category")
-print(df["Ciudad"])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# # df4=pd.DataFrame({"Nombres": nombres, "Edades": edades, "Ciudades": ciudades})
+# # print(df4)
 
 # print("-"*60)
+
+# datos={
+#     "Dia":      ["Lunes", "Martes", "Miercoles", "Martes", "Viernes", np.nan, "Viernes" ],
+#     "Ventas":   [   100,    150,     np.nan,      150,      200,      130,     200      ],
+#     "Vendedor": ["Ana",   "Juan",   "Ana",       "Juan",    "Ana",    "Maria", "Ana"    ],
+#     "Ciudad":   ["Bogota","Cucuta",  np.nan,     "Cucuta",  "Bogota", "Bogota","Bogota",]
+# }
+
+# df=pd.DataFrame(datos)
+# # print(df)
+
+
+# # print(df.head())
+# # print(df.describe())
+# # print(df["Ventas"])
+# # # print(df["Ciudad"])
+# # print(df.iloc[2])
+# # print(df.iloc[5])
+# # print(df[ df["Ventas"] > 150 ])
+# # print(df[ df["Ciudad"] == "Cucuta" ])
+# # print(df.loc[ df["Ventas"] > 100, ["Dia", "Ventas"]])
+# # print("-"*30)
+# # print(df.iloc[ 1 :3, 1:3  ])
+# # print(df.groupby("Vendedor")["Ventas"].sum())
+# # print("-"*30)
+# # print(df.isnull().sum())
+
+# # print("-"*30)
+
+# # print(df)
+# print("-"*30)
+# # df_sin_nulos=df.dropna()
+# # print(df_sin_nulos)
+
+# # df["Ventas"]=df["Ventas"].fillna(df["Ventas"].mean())
+# # # print(df)
+
+# # df["Ventas"]=df["Ventas"].astype(int)
+# # # print(df)
+
+# # df=df.drop_duplicates()
+# # print(df)
+
+# df["Ciudad"]=df["Ciudad"].astype("category")
+# print(df["Ciudad"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # print("-"*60)
---
+++ python/pandas/limpiezaDatosPandas.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+
+data={
+    "Fechas": ["2024-01-01", "2024-01-02", np.nan,"2024-01-02","2024-01-03", "2024-01-03"],
+    "Ventas": [100,200,200, np.nan, 150, 150],
+    "Vendedores": ["Juan", np.nan, "Ana", "Juan", "Ana", "Ana"],
+    "Ciudades": ["Cuauta", "Cucuta", "Bogota", "Cucuta", "Cucuta", "Cucuta"]
+}
+
+df=pd.DataFrame(data)
+print(df)
+
+print(df.isnull().sum())
+
+df_dropped=df.dropna()
+print(df_dropped)
+
+df=df.dropna(subset=["Fechas"])
+print(df)
