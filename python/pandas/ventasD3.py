31a501bb44633bbb6c0bb3085ac6d77342d43078
Milton Figueredo Miles <gratno@gmail.com>
2024-08-29 19:36:03 -0500

    DataFrame Pandas

--- python/pandas/ventasD3.py
+++ python/pandas/ventasD3.py
@@ -5,6 +5,10 @@ datos_ventas={"Día":    ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"],
               "Ventas": [450,         300,      200,      400,         600  ]}
 
 df_ventas =pd.DataFrame(datos_ventas)
+df_ventas.to_csv("ventas_semanales3.csv", index=False)
+
+df_ventas=pd.read_csv("ventas_semanales3.csv")
+print("Datos de ventas leídos del CSV:\n", df_ventas)
 
 
 
@@ -12,10 +16,7 @@ df_ventas =pd.DataFrame(datos_ventas)
 
 
 
-
-
-
-
+# print(df_ventas)
 
 
 
