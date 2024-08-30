a86a5a1e0800ab0042e7c704acc054823fb445aa
Milton Figueredo Miles <gratno@gmail.com>
2024-08-29 19:49:07 -0500

    DataFrame Pandas

--- python/pandas/ventasD3.py
+++ python/pandas/ventasD3.py
@@ -10,12 +10,16 @@ df_ventas.to_csv("ventas_semanales3.csv", index=False)
 df_ventas=pd.read_csv("ventas_semanales3.csv")
 print("Datos de ventas leídos del CSV:\n", df_ventas)
 
+total_ventas=df_ventas["Ventas"].sum()
+promedio_ventas=df_ventas["Ventas"].mean()
+print("\nTotal de Ventas en la Semana:", total_ventas)
+print("Promedio de Ventas Diarias:", promedio_ventas)
 
+ventas_altas=df_ventas[df_ventas["Ventas"] > 300]
+print("\nDías con ventas Mayores a 300:\n", ventas_altas)
 
-
-
-
-
+df_ventas.loc[df_ventas["Día"] == "Lunes", "Ventas"] = 500
+print("\nVentas Actualizadas:\n", df_ventas)
 # print(df_ventas)
 
 
