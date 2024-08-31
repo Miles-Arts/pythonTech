5cedd1872fa644f1d0c89213932a52f568791d6a
Milton Figueredo Miles <gratno@gmail.com>
2024-08-31 17:47:44 -0500

    Retos Pyhton

---
+++ python/ejercicios/frase.py
@@ -0,0 +1,34 @@
+entrada=(input("Ingrese una frase: "))
+letra=(input("Elija una letra: "))
+
+salida = entrada.count(letra)
+
+print(salida)
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
