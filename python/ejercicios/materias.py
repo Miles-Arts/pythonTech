d4fadb34e414385cc30c44838166c0fa93b17223
Milton Figueredo Miles <gratno@gmail.com>
2024-09-01 11:05:39 -0500

    Reto level hard

--- python/ejercicios/materias.py
+++ python/ejercicios/materias.py
@@ -1,59 +1,76 @@
-# def materias():
-#     materias={}
-#     continuar="s"
-#     while continuar == "s":
-#     #     print("Ingrese las materias: ")
-#     #     materia=str(input("Ingrese materias: "))
-#         continuar1="s"
-#         lista=[]
-#         while continuar1 == "s":
-#                 materia=str(input("Escriba Materia: "))
-#                 nota=float(input("Ingrese nota: "))
-#                 lista.append((materia,nota))
-#                 materias[lista]
-#         continuar1=input("Agregar otra materia? (S/N)")
-#     return materias    
+# # def materias():
+# #     materias={}
+# #     continuar="s"
+# #     while continuar == "s":
+# #     #     print("Ingrese las materias: ")
+# #     #     materia=str(input("Ingrese materias: "))
+# #         continuar1="s"
+# #         lista=[]
+# #         while continuar1 == "s":
+# #                 materia=str(input("Escriba Materia: "))
+# #                 nota=float(input("Ingrese nota: "))
+# #                 lista.append((materia,nota))
+# #                 materias[lista]
+# #         continuar1=input("Agregar otra materia? (S/N)")
+# #     return materias    
 
 
-# materias()
-
-
-
+# # materias()
 
 
 
 
 
-def agregar_materias():
-    # cantidad=int(input("Ingrese cantidad materias: "))
-    # for _ in range(cantidad):
+
+
+
+# def agregar_materias():
+#     # cantidad=int(input("Ingrese cantidad materias: "))
+#     # for _ in range(cantidad):
     
-    materias={}
-    continuar = "s"
-    while continuar == "s":
-        print("Ingrese materias")
-        materia=str(input("Ingrese la materia: "))
-        nota=float(input("Ingrese la nota: "))
-        materias[materia]=(nota)
-        continuar=input("Desea continuar (S/N) ")
-        # can_materias={"materia": materia, "nota": nota}
-        # materias.append(can_materias)
-    return materias
+#     materias={}
+#     continuar = "s"
+#     while continuar == "s":
+#         print("Ingrese materias")
+#         materia=str(input("Ingrese la materia: "))
+#         nota=float(input("Ingrese la nota: "))
+#         materias[materia]=(nota)
+#         continuar=input("Desea continuar (S/N) ")
+#         # can_materias={"materia": materia, "nota": nota}
+#         # materias.append(can_materias)
+#     return materias
 
-def imprimir(materias):
-     print("\n ---Materias--- \n")
-     print(materias)
-    #  for materia in materias:
-    #      print(materia, materias) 
+# def imprimir(materias):
+#      print("\n ---Materias--- \n")
+#      print(materias)
+#     #  for materia in materias:
+#     #      print(materia, materias) 
         
-materia_notas= agregar_materias()
-imprimir(materia_notas)
+# materia_notas= agregar_materias()
+# imprimir(materia_notas)
 
 
+materias = [
+    "Matemáticas", 
+    "Física", 
+    "Química", 
+    "Historia", 
+    "Lenguaje", 
+    "Programacóin"
+]
 
+notas=[]
 
+for materia in materias:
+    nota=float(input(F"Ingrese la nota de {materia}: "))
+    notas.append(nota)
 
+promedio=  ma 
 
+print("\nResumen de notas:")
+for i in range(len(materias)):
+    print(f"En {materias[i]} has sacado {notas[i]}")
+    print()
 
 
 
