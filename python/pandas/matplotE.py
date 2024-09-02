f46bf2ece776d361b76108e588e978c3f2dce5dc
Milton Figueredo Miles <gratno@gmail.com>
2024-09-02 07:11:43 -0500

    matplotlib

--- python/pandas/matplotE.py
+++ python/pandas/matplotE.py
@@ -2,14 +2,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+np.random.seed(0)
 
+fechas=pd.date_range("20240101", periods=6)
+# # print(fechas)
 
+valores=np.random.randn(6,4)
 
+# datos=pd.DataFrame(np.random.randn(6,4))
+# print(datos)
 
+columnas=list("ABCD")
+print(columnas)
 
-
-
-
+datos=pd.DataFrame(valores, index=fechas, columns=columnas)
+print(datos)
 
 
 
