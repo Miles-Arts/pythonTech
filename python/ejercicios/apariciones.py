645946aa4a81e579873d6d5e305820ad75b5a0a0
Milton Figueredo Miles <gratno@gmail.com>
2024-09-02 09:51:05 -0500

    Retos Python

---
+++ python/ejercicios/apariciones.py
@@ -0,0 +1,38 @@
+# Enunciado
+
+# Apariciones: 
+# Realizar un algoritmo que reciba una cadena 
+# y devuelva un diccionario con la cantidad 
+# de apariciones de cada palabra en la cadena. 
+
+# Por ejemplo, si recibe "Qué lindo día que hace hoy" 
+# debe devolver: 
+# 'que': 2, 'lindo': 1, 'día': 1, 'hace': 1, 'hoy': 1
+
+
+
+
+cadena_texto=str(input("Ingrese una frase: "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
