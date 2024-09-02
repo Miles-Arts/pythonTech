5c4d174e2c8918bf5948ab01c8bae3c6a47964db
Milton Figueredo Miles <gratno@gmail.com>
2024-09-02 12:23:53 -0500

    Retos Python

--- python/ejercicios/apariciones.py
+++ python/ejercicios/apariciones.py
@@ -37,16 +37,6 @@ for palabra, cantidad in apariciones.items():
 
 
 
-# VOWELS = 'aeiou'
-
-# enum_vowels = {}
-
-# for i, vowel in enumerate(VOWELS, start=1):
-#     enum_vowels[vowel] = i
-#     print(enum_vowels)
-
-
-
 
 
 
