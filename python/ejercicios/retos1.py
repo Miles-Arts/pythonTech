11d416236cddcbdbdf6a446cefcf6b4e790e1a84
Milton Figueredo Miles <gratno@gmail.com>
2024-09-04 07:03:08 -0500

    Retos Python

--- python/ejercicios/retos1.py
+++ python/ejercicios/retos1.py
@@ -2,25 +2,16 @@ peso=float(input("Ingrese peso tornillo en gramos: "))
 
 if peso >= 1 and peso < 3:
     print("El tornillo es ligero")
-elif  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+elif  peso >= 3 and peso < 5 :
+    print("El tornillo es medio")
+elif  peso >= 5 and peso < 7.5 :
+    print("El tornillo es pesado")
+elif  peso >= 7.5 and peso <= 10 :
+    print("El tornillo es muy pesado")
+elif  peso > 10 :
+    print("El tornillo es extremadamente pesado.") 
+else:
+    print("El peso ingresado no es válido.")   
 
 
 
