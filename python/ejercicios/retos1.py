982a1c5f6355441a51e7ccbc985b60be9807bffb
Milton Figueredo Miles <gratno@gmail.com>
2024-09-04 07:44:32 -0500

    Retos Python

--- python/ejercicios/retos1.py
+++ python/ejercicios/retos1.py
@@ -1,17 +1,17 @@
 peso=float(input("Ingrese peso tornillo en gramos: "))
 
 if peso >= 1 and peso < 3:
-    print("El tornillo es ligero")
+    print("El tornillo es ligero.")
 elif  peso >= 3 and peso < 5 :
-    print("El tornillo es medio")
+    print("El tornillo es medio.")
 elif  peso >= 5 and peso < 7.5 :
-    print("El tornillo es pesado")
+    print("El tornillo es pesado.")
 elif  peso >= 7.5 and peso <= 10 :
-    print("El tornillo es muy pesado")
+    print("El tornillo es muy pesado.")
 elif  peso > 10 :
     print("El tornillo es extremadamente pesado.") 
 else:
-    print("El peso ingresado no es válido.")   
+    print("El peso ingresado no es valido.")   
 
 
 
---
+++ python/ejercicios/retos2.py
@@ -0,0 +1,29 @@
+numero=int(input("Grise un número: "))
+
+cuadrado =0
+nivel=0
+
+
+
+if cuadrado == numero:
+    print(f"{numero} es adecuado para apilar en forma cuadrado")
+else:
+    print(f"{numero} no es adecuado para apilar en forma cuadrada.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
