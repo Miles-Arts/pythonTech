0c4e99dbf1a784ad7d0c76a60c7979690244c2ea
Milton Figueredo Miles <gratno@gmail.com>
2024-09-06 06:57:17 -0500

    Retos

--- python/ejercicios/triaangulo.py
+++ python/ejercicios/triaangulo.py
@@ -1,10 +1,27 @@
-print("---Triángulo Rectángulo---")
-altura=int(input("Ingrese la Altura: "))
+# print("---Triángulo Rectángulo---")
+# altura=int(input("Ingrese la Altura: "))
 
-for i in range(1, altura + 1, 2):
-    for j in range(i, 0, -2):
-        print(j, end=" ")
-        # return
-    print()
+# for i in range(1, altura + 1, 2):
+#     for j in range(i, 0, -2):
+#         print(j, end=" ")
+#         # return
+#     print()
 
 
+altura=int(input("Ingrese numero enyero: "))
+           
+           
+if altura % 2 !=0:
+    altura += 1   
+
+for i in range(0, altura,1):
+    print()        
+           
+           
+           
+           
+           
+           
+           
+           
+           
