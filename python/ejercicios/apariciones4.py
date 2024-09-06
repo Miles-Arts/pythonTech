8e19c46588ca69aeee315c44c6a3db75c941df32
Milton Figueredo Miles <gratno@gmail.com>
2024-09-05 21:26:23 -0500

    retos Python

--- python/ejercicios/apariciones4.py
+++ python/ejercicios/apariciones4.py
@@ -1,21 +1,52 @@
-entrada=str(input("Ingrese la frase: ")).lower()
-frase=entrada.split()
-frases={}
+# entrada=str(input("Ingrese la frase: ")).lower()
+# frase=entrada.split()
+# frases={}
 
-for palabra in entrada:
-    if palabra in frases:
-        frases[palabra]+=1
-    else:
-        frases[palabra]=1
+# for palabra in entrada:
+#     if palabra in frases:
+#         frases[palabra]+=1
+#     else:
+#         frases[palabra]=1
 
-print("Palabras Repetidas")
+# print("Palabras Repetidas")
 
-for clave, valor in frases.items():
-    print(f"{clave} y {valor}")
-
-
+# for clave, valor in frases.items():
+#     print(f"{clave} y {valor}")
 
 
+# Escribir un programa que pregunte el nombre del 
+# usuario en la consola y después de que el usuario 
+# lo introduzca muestre por pantalla la cadena 
+# ¡Hola <nombre>!, donde 
+# <nombre> es el nombre que el usuario haya introducido.
+
+# ingrese_nombre=str(input("Hola, Ingresa tu nombre: "))
+
+# print(f"¡Hola {ingrese_nombre}!")
+
+
+# num1=3
+# num2=2
+# num3=2
+# num4=5
+# num5=2
+
+# def operacion():
+#     operacion_salida=  ((num1+num2) / (num3*num4)) ** num5
+#     print(operacion_salida)
+
+
+# operacion()
+
+
+
+
+ingrese_numero=int(input("Ingrese numero entero positivo: "))
+
+for numero in range(0,ingrese_numero,1):
+  
+    numero+=1
+    print(numero)
 
 
 
