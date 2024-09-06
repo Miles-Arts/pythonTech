4781ada9b26d0ee6ac0eafda3ecfd3fe00be45a6
Milton Figueredo Miles <gratno@gmail.com>
2024-09-06 07:58:03 -0500

    Retos

--- python/ejercicios/hexade.py
+++ python/ejercicios/hexade.py
@@ -1,7 +1,28 @@
-numero=int(input("Ingrese un número entero decimal: "))
+decimal=int(input("Ingrese un número entero decimal: "))
+
+absoluto=abs(decimal)
+
+if absoluto == 0:
+    resultado = "0"
+else:
+    hex_char="0123456789ABCDEF"
+    hexadecimal=[]
+
+    while absoluto > 0:
+
+        hexadecimal.append(hex_char[absoluto % 16])
+        # hexadecimal.append(str[absoluto % 2])
+        absoluto//=16
+        # absoluto//=2
+
+
+    resultado= "".join(reversed(hexadecimal))    
 
 
 
+if decimal < 0:
+    resultado= "-" + resultado    
+print(f"El equivalente hexadecimal es: {resultado}")
 
 
 
