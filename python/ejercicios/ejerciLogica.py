944b68618768c0fc8d309757b5a5a2d9657b284b
Milton Figueredo Miles <gratno@gmail.com>
2024-09-17 18:02:14 -0500

    Python min max

--- python/ejercicios/ejerciLogica.py
+++ python/ejercicios/ejerciLogica.py
@@ -1150,7 +1150,16 @@
 # print(f"El valor mínimo es: {precios_min}")
 
 
+precios=[50,75,46,22,80,65,8]
 
+min = max = precios[0]
+for precio in precios:
+    if precio < min:
+        min = precio
+    elif precio > max:
+        max = precio
+print(f"El mínimo es {min}")            
+print(f"El máximo es {max}")            
 
 
 
