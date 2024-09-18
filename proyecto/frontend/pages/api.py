757710ed4cca7ab84361f41cc2bf3d8a9a78a1bb
Milton Figueredo Miles <gratno@gmail.com>
2024-09-18 06:57:57 -0500

    Python data

--- proyecto/frontend/pages/api.py
+++ proyecto/frontend/pages/api.py
@@ -2,9 +2,13 @@ import csv
 import os #MANEJO CARPETAS
 import time #CAMBIAR NOMBRE ARCHIVO
 from flask import Flask, jsonify, request, send_from_directory, send_file
+from flask_cors import CORS
 
+# pip install Flask
+# pip install Flask-Cors
 
-
+app=Flask(__name__)
+CORS()
 
 
 
