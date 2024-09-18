7fcaad2ca04aad3f28b4cbb6af05d886af4adf15
Milton Figueredo Miles <gratno@gmail.com>
2024-09-18 06:52:54 -0500

    Python data

---
+++ proyecto/frontend/pages/api.py
@@ -0,0 +1,36 @@
+import csv
+import os #MANEJO CARPETAS
+import time #CAMBIAR NOMBRE ARCHIVO
+from flask import Flask, jsonify, request, send_from_directory, send_file
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
