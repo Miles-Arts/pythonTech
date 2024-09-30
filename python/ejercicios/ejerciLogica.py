bf464d53df63421524c1279565a07d16167ede8e
Milton Figueredo Miles <gratno@gmail.com>
2024-09-29 21:07:19 -0500

    Python ID bank

--- python/ejercicios/ejerciLogica.py
+++ python/ejercicios/ejerciLogica.py
@@ -2934,57 +2934,72 @@ facturas={}
 #             'teléfono': '667677855', 
 #            'descuento': 15.7}}
 
-base_datos={
-            "id": {
-            "nombre": "nombre",
-            "email": "email",
-            "teléfono": "telefono",
-            "descuento": float()
-        }
+# base_datos={
+#             "id": {
+#             "nombre": "nombre",
+#             "email": "email",
+#             "teléfono": "telefono",
+#             "descuento": float()
+#         }
 
-}
+# }
 
-clientes={
-        '01234567L': {
-          'nombre': 'Luis González', 
-           'email': 'luisgonzalez@mail.com', 
-        'teléfono': '656343576', 
-       'descuento': 12.5
-       }, 
+# clientes={
+#         '01234567L': {
+#           'nombre': 'Luis González', 
+#            'email': 'luisgonzalez@mail.com', 
+#         'teléfono': '656343576', 
+#        'descuento': 12.5
+#        }, 
 
-       '71476342J': {
-          'nombre': 'Macarena Ramírez', 
-           'email': 'macarena@mail.com', 
-        'teléfono': '692839321', 
-       'descuento': 8.0
-           }, 
+#        '71476342J': {
+#           'nombre': 'Macarena Ramírez', 
+#            'email': 'macarena@mail.com', 
+#         'teléfono': '692839321', 
+#        'descuento': 8.0
+#            }, 
 
-       '63823376M': {
-          'nombre': 'Juan José Martínez', 
-           'email': 'juanjo@mail.com', 
-        'teléfono': '664888233', 
-       'descuento': 5.2
-           }, 
+#        '63823376M': {
+#           'nombre': 'Juan José Martínez', 
+#            'email': 'juanjo@mail.com', 
+#         'teléfono': '664888233', 
+#        'descuento': 5.2
+#            }, 
 
-       '98376547F': {
-          'nombre': 'Carmen Sánchez', 
-           'email': 'carmen@mail.com', 
-        'teléfono': '667677855', 
-       'descuento': 15.7
-           }
-        }
-
-
-
-base_datos[id]=clientes
-
-print(base_datos["id"])
+#        '98376547F': {
+#           'nombre': 'Carmen Sánchez', 
+#            'email': 'carmen@mail.com', 
+#         'teléfono': '667677855', 
+#        'descuento': 15.7
+#            }
+#         }
 
 
 
+# base_datos[id]=clientes
+
+# print(base_datos["id"])
 
 
+# DATOS CLIENTES
 
+datos_clientes= "nif;nombre;email;teléfono;descuento\n01234567L;Luis González;luisgonzalez@mail.com;656343576;12.5\n71476342J;Macarena Ramírez;macarena@mail.com;692839321;8\n63823376M;Juan José Martínez;juanjo@mail.com;664888233;5.2\n98376547F;Carmen Sánchez;carmen@mail.com;667677855;15.7"
+
+lista_clientes = datos_clientes.split("\n")
+directorio={}
+lista_campos= lista_clientes[0].split(";")
+
+for i in lista_clientes[1:]:
+    cliente={}
+    lista_info=i.split(";")
+
+    for j in range(1,len(lista_campos)):
+        if lista_campos[j] == "descuento":
+            lista_info[j]= float(lista_info[j])
+            cliente[lista_campos[j]] = lista_info[j]
+    directorio[lista_info[0]]=cliente
+
+print(directorio)    
 
 
 
