a4ff820bfacad70006b1dd1bf39db826b067a8e3
Milton Figueredo Miles <gratno@gmail.com>
2025-01-26 20:01:46 -0500

    Python 2025

---
+++ python/ejercicios/gptLogica.py
@@ -0,0 +1,135 @@
+# print("Ingresa 3 numeros: ")
+# numero1=float(input("primer número: "))
+# numero2=float(input("segundo número: "))
+# numero3=float(input("tercer número: "))
+
+# suma=numero1+numero2+numero3
+# promedio=suma / 3
+
+# print("---Resultados---")
+# print(f"La suma de los 3 números es: {suma:.2f}")
+# print(f"El promedio de los tres numeros es: {promedio:.1f}")
+
+
+# temperatura=float(input("Ingresa la termperatura en grados Celsius: "))
+
+# fahrenheit=(temperatura*9/5) + 32
+# kelvin=temperatura + float(273.15)
+
+# print("---Conversor de temperaturas---")
+# print(f"Temperatura en Celsius es: {temperatura} °C")
+# print(f"Temperatura en Fahrenheit es: {fahrenheit} °F")
+# print(f"Temperatura en Kelvin es: {kelvin:.2f} °K")
+
+
+print("Ingresa las dimenciones del triángulo: ")
+base=float(input("Ancho: "))
+altura=float(input("Altura: "))
+print("\nIngresa las dimenciones de los bordes del triangulo: ")
+perimetro1=float(input("Medida base: "))
+perimetro2=float(input("Medida izquierda: "))
+perimetro3=float(input("Medida base: "))
+
+area=float(0.5) * base * altura
+area1=(base * altura) / 2
+
+perimetro_total=perimetro1+perimetro2+perimetro3
+
+
+print(f"El área del triángulo es: {area1:.2f}")
+print(f"El perímetro del triángulo es: {perimetro_total:.2f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
