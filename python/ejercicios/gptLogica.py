40669d984aa7a0f4b2eec16b4f9d9f56b3176292
Milton Figueredo Miles <gratno@gmail.com>
2025-02-02 17:06:08 -0500

    Python listas

--- python/ejercicios/gptLogica.py
+++ python/ejercicios/gptLogica.py
@@ -325,40 +325,41 @@
 #     print(f"{items["categoria"]}: {porcentaje_gasto:.2f}%")
 
 
-def cualcular_porcentaje(gasto, total):
-    return (gasto / total) * 100 if total > 0 else 0
+# def cualcular_porcentaje(gasto, total):
+#     return (gasto / total) * 100 if total > 0 else 0
 
-print("Calculadora de gastos mensuales")
-nombre=input("Ingresa tu nombre: ").title()
+# print("Calculadora de gastos mensuales")
+# nombre=input("Ingresa tu nombre: ").title()
+# lista_gastos=int(input("Ingrese numero de gastos: "))
 
-gastos=[]
+# gastos=[]
 
-for i in range(4):
+# for i in range(lista_gastos):
 
-    categoria= input(f"Ingresa el nombre del gasto {i+1}")
-    monto=float(input(f"Ingrese el monto gastado en {categoria}: $"))
-    gastos.append((categoria,monto))
+#     categoria= input(f"Ingresa el nombre del gasto {i+1}: ").title()
+#     monto=float(input(f"Ingrese el monto gastado en {categoria}: $"))
+#     gastos.append((categoria,monto))
 
-total_gastos=sum(gasto[1] for gasto in gastos)
-gasto_mayor=max(gastos, key=lambda x: x[1] )    
-gasto_minimo=min(gastos, key=lambda x: x[1])
-gastos_ordenados=sorted(gastos, key=lambda x: x[1])
+# total_gastos=sum(gasto[1] for gasto in gastos)
+# gasto_mayor=max(gastos, key=lambda x: x[1] )    
+# gasto_minimo=min(gastos, key=lambda x: x[1])
+# gastos_ordenados=sorted(gastos, key=lambda x: x[1])
 
-print(f"\nResumen de tus gastos")
-print(f" Usuario: {nombre}\n")
+# print(f"\nResumen de tus gastos")
+# print(f" Usuario: {nombre}\n")
 
-for categoria, monto in gastos:
-    porcentaje=cualcular_porcentaje(monto, total_gastos)
-    print(f"- {categoria}: ${monto:.2f} ({porcentaje:2f}%)")
+# for categoria, monto in gastos:
+#     porcentaje=cualcular_porcentaje(monto, total_gastos)
+#     print(f"- {categoria}: ${monto:.2f} ({porcentaje:2f} %)")
 
 
-print(f"\n Total gastado: ${total_gastos:.2f}")
-print(f"Gasto más alto: {gasto_mayor[0] } (${gasto_mayor[1]:.2f})")
-print(f"Gasto más bajo: {gasto_minimo[0] } (${gasto_minimo[1]:.2f})")
-print(f"\n Gastos ordenados de menor a mayor: ")
+# print(f"\n Total gastado: ${total_gastos:.2f}")
+# print(f"Gasto más alto: {gasto_mayor[0] } (${gasto_mayor[1]:.2f})")
+# print(f"Gasto más bajo: {gasto_minimo[0] } (${gasto_minimo[1]:.2f})")
+# print(f"\n Gastos ordenados de menor a mayor: ")
 
-for categoria, monto in gastos_ordenados:
-    print(f"- {categoria}: ${monto:.2f}")
+# for categoria, monto in gastos_ordenados:
+#     print(f"- {categoria}: ${monto:.2f}")
 
 
 
