054326fb48cc46d052f9204edd3591d7f5731974
Milton Figueredo <gratno@gmail.com>
2025-03-20 12:50:18 -0500

    Python

--- python/ejercicios/gptLogica.py
+++ python/ejercicios/gptLogica.py
@@ -2420,18 +2420,58 @@ import random
 
 
 
+print("---Clasificador de Libros y Marcas de Autos---")
+
+lista_nombres=["Mercedes Benz",
+                    "Audi",
+                    "Porsche",
+                    "Maserati",
+                    "BMW",
+                    "Ferrari",
+                    "Lexus",
+                    "Toyota",
+                    "Chevrolet",
+                    "Jeep",
+                    "El Hombre en busca de Sentido",
+                    "Don Quijote",
+                    "A Tale of Two Cities",
+                    "The Little Prince",
+                    "Harry Potter and the Philosopher's Stone",
+                    "And Then There Were None",
+                    "The Hobbit",
+                    "Alice's Adventures in Wonderland",
+                    "The Lord of the Rings",
+                    "Aprende Python" 
+                    ]
+
+def clasificador(lista):
+    carros=[]
+    libros=[]
+
+    lista_general=random.sample(lista_nombres, k=min(10, len(lista_nombres)))
 
 
+    for nombres in lista_general:
+        if nombres in ["Mercedes Benz","Audi","Porsche","Maserati","BMW","Ferrari","Lexus","Toyota","Chevrolet","Jeep"]:
+            carros.append(nombres)
+        elif nombres in  [   "El Hombre en busca de Sentido","Don Quijote","A Tale of Two Cities","The Little Prince","Harry Potter and the Philosopher's Stone","And Then There Were None","The Hobbit","Alice's Adventures in Wonderland","The Lord of the Rings","Aprende Python" ]:
+            libros.append(nombres)
 
+    return carros, libros
 
+# pares, impares=clasificar_numeros(numeros)
 
+carros, libros=clasificador(lista_nombres)
 
+print(f"\nLa lista de libros es:\n")
+for i in libros:
+    print(f"{i}")
 
+print(f"\nLa lista de carros es:\n")
+for j in carros:
+    print(f"{j}")
 
-
-
-
-
+print(f"\nLista completa de elementos {lista_nombres}")
 
 
 
